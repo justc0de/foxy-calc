8f116b8c37d5897abc0872a02c0cffe044aa9213
Robert Byrne <bobbyrne01@gmail.com>
2013-12-22 20:36:15 +0000

    #1 Removed additional variable and updated addon description

--- data/js/UserInteraction.js
+++ data/js/UserInteraction.js
@@ -3,146 +3,110 @@ window.addEventListener('click', function(event) {
     if (event.target.id == '7'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"7";
-	  document.getElementById("inputbox").value = x;
-    }
-    else if (event.target.id == '8'){
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"7";
+    
+    } else if (event.target.id == '8'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"8";
-	  document.getElementById("inputbox").value = x;
-    }
-    else if (event.target.id == '9'){
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"8";
+    
+    } else if (event.target.id == '9'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"9";
-	  document.getElementById("inputbox").value = x;
-    }
-    else if (event.target.id == '4'){
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"9";
+    
+    } else if (event.target.id == '4'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"4";
-	  document.getElementById("inputbox").value = x;
-    }
-    else if (event.target.id == '5'){
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"4";
+    
+    } else if (event.target.id == '5'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"5";
-	  document.getElementById("inputbox").value = x;
-    }
-    else if (event.target.id == '6'){
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"5";
+    
+    } else if (event.target.id == '6'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"6";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"6";
 		  
-    }
-    else if (event.target.id == '1'){
+    } else if (event.target.id == '1'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"1";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"1";
 		  
-    }
-    else if (event.target.id == '2'){
+    } else if (event.target.id == '2'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"2";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"2";
 		  
-    }
-    else if (event.target.id == '3'){
+    } else if (event.target.id == '3'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"3";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"3";
 		  
-    }
-    else if (event.target.id == '0'){
+    } else if (event.target.id == '0'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"0";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"0";
 		  
-    }
-    else if (event.target.id == 'decimal'){
+    } else if (event.target.id == 'decimal'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+".";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+".";
 		  
-    }
-    else if (event.target.id == '2power'){
+    } else if (event.target.id == '2power'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"^2";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"^2";
 		  
-    }
-    else if (event.target.id == '('){
+    } else if (event.target.id == '('){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+"(";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"(";
 		  
-    }
-    else if (event.target.id == ')'){
+    } else if (event.target.id == ')'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+")";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+")";
 		  
-    }
-    else if (event.target.id == 'delete'){
+    } else if (event.target.id == 'delete'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value.slice(0,-1);
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value.slice(0,-1);
 		  
-    }
-    else if (event.target.id == 'allclear'){
+    } else if (event.target.id == 'allclear'){
 		  
 	  console.log(event.target.id);
 	  document.getElementById("inputbox").value = "";
 		  
-    }
-    else if (event.target.id == 'multiply'){
+    } else if (event.target.id == 'multiply'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+" * ";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+" * ";
 		  
-    }
-    else if (event.target.id == 'divide'){
+    } else if (event.target.id == 'divide'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+" / ";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+" / ";
 		  
-    }
-    else if (event.target.id == 'plus'){
+    } else if (event.target.id == 'plus'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+" + ";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+" + ";
 		  
-    }
-    else if (event.target.id == 'minus'){
+    } else if (event.target.id == 'minus'){
 		  
 	  console.log(event.target.id);
-	  var x =document.getElementById("inputbox").value+" - ";
-	  document.getElementById("inputbox").value = x;
+	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+" - ";
 		  
-    }
-    else if (event.target.id == 'answer'){
+    } else if (event.target.id == 'answer'){
 		  
 	  console.log(event.target.id);
 		  
-    }		
-    else if (event.target.id == 'equals'){
+    } else if (event.target.id == 'equals'){
 		  
       FoxyCalc_Panel.equals();
-		  
     }
 }, false);
 
