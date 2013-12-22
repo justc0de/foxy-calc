e2e9abdce24ded66798d73f54528ec2135bcc1b4
dillbyrne <dillbyrne@users.noreply.github.com>
2013-12-22 15:56:43 +0000

    Added listeners to the buttons

--- data/js/UserInteraction.js
+++ data/js/UserInteraction.js
@@ -1,10 +1,99 @@
 window.addEventListener('click', function(event) {
-	
-  if (event.target.id.indexOf('equals') == 0){
-		
-    FoxyCalc_Panel.equals();
-		
-  }
+
+    if (event.target.id.indexOf('7') == 0){
+		  
+	  console.log(event.target.id);
+    }
+    else if (event.target.id.indexOf('8') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('9') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('4') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('5') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('6') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('1') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('2') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('3') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('0') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('decimal') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('delete') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('allclear') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('multiply') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('divide') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('plus') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('minus') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }
+    else if (event.target.id.indexOf('answer') == 0){
+		  
+	  console.log(event.target.id);
+		  
+    }		
+    else if (event.target.id.indexOf('equals') == 0){
+		  
+      FoxyCalc_Panel.equals();
+		  
+    }
 }, false);
 
 // functions available to Panel
