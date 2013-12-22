0bee910b7e1854f80634d7dcce9af8ac1e6e98d7
dillbyrne <dillbyrne@users.noreply.github.com>
2013-12-22 19:27:58 +0000

    Most buttons now affect the input box. Answer has to be done and delete needs to be fine tuned

--- data/js/UserInteraction.js
+++ data/js/UserInteraction.js
@@ -1,105 +1,145 @@
 window.addEventListener('click', function(event) {
 
-    if (event.target.id.indexOf('7') == 0){
+    if (event.target.id == '7'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+"7";
+	  document.getElementById("inputbox").value = x;
     }
-    else if (event.target.id.indexOf('8') == 0){
+    else if (event.target.id == '8'){
 		  
 	  console.log(event.target.id);
-		  
+	  var x =document.getElementById("inputbox").value+"8";
+	  document.getElementById("inputbox").value = x;
     }
-    else if (event.target.id.indexOf('9') == 0){
+    else if (event.target.id == '9'){
 		  
 	  console.log(event.target.id);
-		  
+	  var x =document.getElementById("inputbox").value+"9";
+	  document.getElementById("inputbox").value = x;
     }
-    else if (event.target.id.indexOf('4') == 0){
+    else if (event.target.id == '4'){
 		  
 	  console.log(event.target.id);
-		  
+	  var x =document.getElementById("inputbox").value+"4";
+	  document.getElementById("inputbox").value = x;
     }
-    else if (event.target.id.indexOf('5') == 0){
+    else if (event.target.id == '5'){
 		  
 	  console.log(event.target.id);
-		  
+	  var x =document.getElementById("inputbox").value+"5";
+	  document.getElementById("inputbox").value = x;
     }
-    else if (event.target.id.indexOf('6') == 0){
+    else if (event.target.id == '6'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+"6";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('1') == 0){
+    else if (event.target.id == '1'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+"1";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('2') == 0){
+    else if (event.target.id == '2'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+"2";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('3') == 0){
+    else if (event.target.id == '3'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+"3";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('0') == 0){
+    else if (event.target.id == '0'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+"0";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('decimal') == 0){
+    else if (event.target.id == 'decimal'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+".";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('(') == 0){
+    else if (event.target.id == '2power'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+"^2";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf(')') == 0){
+    else if (event.target.id == '('){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+"(";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('delete') == 0){
+    else if (event.target.id == ')'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+")";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('allclear') == 0){
+    else if (event.target.id == 'delete'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value.slice(0,-1);
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('multiply') == 0){
+    else if (event.target.id == 'allclear'){
 		  
 	  console.log(event.target.id);
+	  document.getElementById("inputbox").value = "";
 		  
     }
-    else if (event.target.id.indexOf('divide') == 0){
+    else if (event.target.id == 'multiply'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+" * ";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('plus') == 0){
+    else if (event.target.id == 'divide'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+" / ";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('minus') == 0){
+    else if (event.target.id == 'plus'){
 		  
 	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+" + ";
+	  document.getElementById("inputbox").value = x;
 		  
     }
-    else if (event.target.id.indexOf('answer') == 0){
+    else if (event.target.id == 'minus'){
+		  
+	  console.log(event.target.id);
+	  var x =document.getElementById("inputbox").value+" - ";
+	  document.getElementById("inputbox").value = x;
+		  
+    }
+    else if (event.target.id == 'answer'){
 		  
 	  console.log(event.target.id);
 		  
     }		
-    else if (event.target.id.indexOf('equals') == 0){
+    else if (event.target.id == 'equals'){
 		  
       FoxyCalc_Panel.equals();
 		  
