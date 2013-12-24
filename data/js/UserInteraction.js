ca18bd28cd8338e59bd3d72a9b00d10bc6946cc1
Robert Byrne <bobbyrne01@gmail.com>
2013-12-24 01:12:53 +0000

    #1 Ans functionality implemented

--- data/js/UserInteraction.js
+++ data/js/UserInteraction.js
@@ -1,108 +1,89 @@
 window.addEventListener('click', function(event) {
 
     if (event.target.id == '7'){
-		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"7";
+
+	  document.getElementById("inputbox").value += "7";
     
     } else if (event.target.id == '8'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"8";
+	  document.getElementById("inputbox").value += "8";
     
     } else if (event.target.id == '9'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"9";
+	  document.getElementById("inputbox").value += "9";
     
     } else if (event.target.id == '4'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"4";
+	  document.getElementById("inputbox").value += "4";
     
     } else if (event.target.id == '5'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"5";
+	  document.getElementById("inputbox").value += "5";
     
     } else if (event.target.id == '6'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"6";
+	  document.getElementById("inputbox").value += "6";
 		  
     } else if (event.target.id == '1'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"1";
+	  document.getElementById("inputbox").value += "1";
 		  
     } else if (event.target.id == '2'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"2";
+	  document.getElementById("inputbox").value += "2";
 		  
     } else if (event.target.id == '3'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"3";
+	  document.getElementById("inputbox").value += "3";
 		  
     } else if (event.target.id == '0'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"0";
+	  document.getElementById("inputbox").value += "0";
 		  
     } else if (event.target.id == 'decimal'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+".";
+	  document.getElementById("inputbox").value += ".";
 		  
     } else if (event.target.id == '2power'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"^2";
+	  document.getElementById("inputbox").value += "^2";
 		  
     } else if (event.target.id == '('){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"(";
+	  document.getElementById("inputbox").value += "(";
 		  
     } else if (event.target.id == ')'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+")";
+	  document.getElementById("inputbox").value += ")";
 		  
     } else if (event.target.id == 'delete'){
 		  
-	  console.log(event.target.id);
 	  document.getElementById("inputbox").value = document.getElementById("inputbox").value.slice(0,-1);
 		  
     } else if (event.target.id == 'allclear'){
 		  
-	  console.log(event.target.id);
 	  document.getElementById("inputbox").value = "";
+	  FoxyCalc_Panel.setAnsValue("");
 		  
     } else if (event.target.id == 'multiply'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"*";
+	  document.getElementById("inputbox").value += "*";
 		  
     } else if (event.target.id == 'divide'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"/";
+	  document.getElementById("inputbox").value += "/";
 		  
     } else if (event.target.id == 'plus'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"+";
+	  document.getElementById("inputbox").value += "+";
 		  
     } else if (event.target.id == 'minus'){
 		  
-	  console.log(event.target.id);
-	  document.getElementById("inputbox").value = document.getElementById("inputbox").value+"-";
+	  document.getElementById("inputbox").value += "-";
 		  
     } else if (event.target.id == 'answer'){
 		  
-	  console.log(event.target.id);
+	  document.getElementById("inputbox").value += FoxyCalc_Panel.getAnsValue();
 		  
     } else if (event.target.id == 'equals'){
 		  
@@ -121,6 +102,35 @@ window.addEventListener('click', function(event) {
 
 // functions available to Panel
 var FoxyCalc_Panel = {
+		
+  ansValue: "",
+  
+  getAnsValue: function() {
+	  
+	// try .. catch, in case Ans is first button clicked
+	try{
+		
+      // check if Ans has been set or cleared
+	  if (ansValue === ""){
+	  
+	    FoxyCalc_Panel.setStatus("Ans is not set or has been cleared", 3500);
+	    return "";
+	
+	  }else{
+	
+	    return ansValue;
+	  }
+	
+	}catch(err){
+		  
+	  FoxyCalc_Panel.setStatus("Ans is not set or has been cleared", 3500);
+	  return "";
+	}
+  },
+  
+  setAnsValue: function(value) {
+	  ansValue = value;
+  },
 
   equals: function() {
 	
@@ -136,6 +146,7 @@ var FoxyCalc_Panel = {
 	  try {
 	
         document.getElementById("inputbox").value = math.eval(document.getElementById("inputbox").value);
+        FoxyCalc_Panel.setAnsValue(document.getElementById("inputbox").value);
 	  
 	  } catch(err) {
 
